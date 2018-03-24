a75a620cef02f5acbe925aaca021b97c4b559b7a
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 10:01:03 -0400

    UsersApiFeatureTest add assertion for get all users response status code

--- src/test/java/com/example/nyccityrecord/features/UsersApiFeatureTest.java
+++ src/test/java/com/example/nyccityrecord/features/UsersApiFeatureTest.java
@@ -1,12 +1,20 @@
 package com.example.nyccityrecord.features;
 
+import com.example.nyccityrecord.models.User;
 import com.example.nyccityrecord.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -23,5 +31,33 @@ public class UsersApiFeatureTest {
     @After
     public void tearDown() {
         userRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldAllowFullCrudForAUser() throws Exception {
+
+        User firstUser = new User(
+            "someone",
+            "Ima",
+            "Person"
+        );
+
+        User secondUser = new User(
+            "someone_else",
+            "Someone",
+            "Else"
+        );
+
+        Stream.of(firstUser, secondUser)
+            .forEach(user -> {
+                userRepository.save(user);
+            });
+
+        when()
+            .get("http://localhost:8080/users/")
+            .then()
+            .statusCode(is(200))
+            .and().body(containsString("someone"))
+            .and().body(containsString("Else"));
     }
 }
