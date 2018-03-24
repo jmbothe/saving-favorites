f51c20a29a578ead348b806a87c7227cc129ce36
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 11:19:42 -0400

    UserController add DELETE by id route

--- users-api/src/main/java/com/example/usersapi/controllers/UserController.java
+++ users-api/src/main/java/com/example/usersapi/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.example.usersapi.repositories.UserRepository;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -33,6 +30,12 @@ public class UserController {
         }
 
         return foundUser;
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteUserById(@PathVariable Long id) {
+        userRepository.delete(id);
+        return HttpStatus.OK;
     }
 
     //EXCEPTION HANDLERS
