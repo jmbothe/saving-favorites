04aa765b534e5eba0c65de4eb4b8af87028757b8
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 09:56:14 -0400

    add setup and tearDown to UsersApiFeatureTest

--- src/test/java/com/example/nyccityrecord/features/UsersApiFeatureTest.java
+++ src/test/java/com/example/nyccityrecord/features/UsersApiFeatureTest.java
@@ -1,6 +1,10 @@
 package com.example.nyccityrecord.features;
 
+import com.example.nyccityrecord.repositories.UserRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -8,4 +12,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class UsersApiFeatureTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @After
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
 }
