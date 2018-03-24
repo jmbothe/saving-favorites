d02dccaa2bc3c81686a9ea861667593c92cf7fa4
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 13:42:02 -0400

    update users-api User model

--- users-api/src/main/java/com/example/usersapi/models/User.java
+++ users-api/src/main/java/com/example/usersapi/models/User.java
@@ -3,6 +3,7 @@ package com.example.usersapi.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter
@@ -11,7 +12,8 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "USER_ID")
+    private Long userId;
 
     @Column(name = "EMAIL")
     private String email;
@@ -21,6 +23,11 @@ public class User {
 
     @Column(name = "LAST_NAME")
     private String lastName;
+
+    //    @JsonIgnore
+    @OneToMany
+    @JoinColumn(name = "USER_ID")
+    private List<Favorite> favorites;
 
     public User(String email, String firstName, String lastName) {
         this.email = email;
