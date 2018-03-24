1429e367ef9f5ae7c7d5fc4f585a79a9fc172510
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 10:57:10 -0400

    UserControllerTest add GET all assertions

--- users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
+++ users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
@@ -1,11 +1,37 @@
 package com.example.usersapi.controllers;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void findAllUsers_success_returnsStatusOK() throws Exception {
+
+        this.mockMvc
+            .perform(get("/"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findAllUsers_success_returnAllUsersAsJSON() throws Exception {
+
+        this.mockMvc
+            .perform(get("/"))
+            .andExpect(jsonPath("$", hasSize(2)));
+    }
 
 }
