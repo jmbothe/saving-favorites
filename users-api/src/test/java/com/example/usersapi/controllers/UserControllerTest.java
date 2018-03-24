50ec3218522eaa56256ba0cb6f22e04259d9629e
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 11:03:39 -0400

    add to users-api models and repositories

--- users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
+++ users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
@@ -1,13 +1,21 @@
 package com.example.usersapi.controllers;
 
+import com.example.usersapi.models.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +25,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository mockUserRepository;
+
+    @Before
+    public void setUp() {
+        User firstUser = new User(
+            "someone",
+            "Ima",
+            "Person"
+        );
+
+        User secondUser = new User(
+            "someone_else",
+            "Someone",
+            "Else"
+        );
+
+        Iterable<User> mockUsers =
+            Stream.of(firstUser, secondUser).collect(Collectors.toList());
+
+        given(mockUserRepository.findAll()).willReturn(mockUsers);
+    }
 
     @Test
     public void findAllUsers_success_returnsStatusOK() throws Exception {
