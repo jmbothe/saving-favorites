a585ec0af5ab66430efb7ec42c348f2857a1ed4a
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 10:52:02 -0400

    create users-api UserControllerTest

--- users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
@@ -8,13 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 public class UsersApiApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "some users";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApiApplication.class, args);
---
+++ users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.usersapi.controllers;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UsersController.class)
+public class UsersControllerTest {
+
+}
