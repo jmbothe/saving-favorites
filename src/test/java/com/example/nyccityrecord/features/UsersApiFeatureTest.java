eacc993d65761c0587225b2b3cecb05b16d9223e
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 10:26:09 -0400

    UsersApiFeatureTest add POST, PATCH and DELETE assertions

--- src/test/java/com/example/nyccityrecord/features/UsersApiFeatureTest.java
+++ src/test/java/com/example/nyccityrecord/features/UsersApiFeatureTest.java
@@ -12,7 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.Stream;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.Is.is;
 
@@ -53,11 +55,63 @@ public class UsersApiFeatureTest {
                 userRepository.save(user);
             });
 
+        // Test get all Users
         when()
             .get("http://localhost:8080/users/")
             .then()
             .statusCode(is(200))
             .and().body(containsString("someone"))
             .and().body(containsString("Else"));
+
+        // Test creating a User
+        User userNotYetInDb = new User(
+            "new_user",
+            "Not",
+            "Yet Created"
+        );
+
+        given()
+            .contentType(JSON)
+            .and().body(userNotYetInDb)
+            .when()
+            .post("http://localhost:8080/users")
+            .then()
+            .statusCode(is(200))
+            .and().body(containsString("new_user"));
+
+        // Test get all Users again
+        when()
+            .get("http://localhost:8080/users/")
+            .then()
+            .statusCode(is(200))
+            .and().body(containsString("someone"))
+            .and().body(containsString("Else"))
+            .and().body(containsString("Yet Created"));
+
+        // Test finding one user by ID
+        when()
+            .get("http://localhost:8080/users/" + secondUser.getId())
+            .then()
+            .statusCode(is(200))
+            .and().body(containsString("Someone"))
+            .and().body(containsString("Else"));
+
+        // Test updating a user
+        secondUser.setFirstName("changed_name");
+
+        given()
+            .contentType(JSON)
+            .and().body(secondUser)
+            .when()
+            .patch("http://localhost:8080/users/" + secondUser.getId())
+            .then()
+            .statusCode(is(200))
+            .and().body(containsString("changed_name"));
+
+        // Test deleting a user
+        when()
+            .delete("http://localhost:8080/users/" + secondUser.getId())
+            .then()
+            .statusCode(is(200));
     }
 }
