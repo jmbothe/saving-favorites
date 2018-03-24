246077622472531500733b88b0378fd795230159
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 11:08:28 -0400

    UserControllerTest extend and pass GET all assertions

--- users-api/src/main/java/com/example/usersapi/controllers/UserController.java
+++ users-api/src/main/java/com/example/usersapi/controllers/UserController.java
@@ -1,7 +1,18 @@
 package com.example.usersapi.controllers;
 
+import com.example.usersapi.models.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public Iterable<User> findAllUsers() {
+        return userRepository.findAll();
+    }
 }
--- users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
+++ users-api/src/test/java/com/example/usersapi/controllers/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -63,6 +64,30 @@ public class UserControllerTest {
         this.mockMvc
             .perform(get("/"))
             .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void findAllUsers_success_returnEmailForEachUser() throws Exception {
+
+        this.mockMvc
+            .perform(get("/"))
+            .andExpect(jsonPath("$[0].email", is("someone")));
+    }
+
+    @Test
+    public void findAllUsers_success_returnFirstNameForEachUser() throws Exception {
+
+        this.mockMvc
+            .perform(get("/"))
+            .andExpect(jsonPath("$[0].firstName", is("Ima")));
+    }
+
+    @Test
+    public void findAllUsers_success_returnLastNameForEachUser() throws Exception {
+
+        this.mockMvc
+            .perform(get("/"))
+            .andExpect(jsonPath("$[0].lastName", is("Person")));
     }
 
 }
