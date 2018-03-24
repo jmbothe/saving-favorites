56e6c76671b40eb67958f0f1ad2955cb899e68cb
jmbothe <jeffrymbothe@gmail.com>
2018-03-24 11:16:48 -0400

    UserController GET by id route - add exception handling

--- users-api/src/main/java/com/example/usersapi/controllers/UserController.java
+++ users-api/src/main/java/com/example/usersapi/controllers/UserController.java
@@ -2,10 +2,16 @@ package com.example.usersapi.controllers;
 
 import com.example.usersapi.models.User;
 import com.example.usersapi.repositories.UserRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class UserController {
@@ -18,7 +24,24 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User findUserById(@PathVariable Long id) {
-        return userRepository.findOne(id);
+    public User findUserById(@PathVariable Long id) throws NotFoundException {
+
+        User foundUser = userRepository.findOne(id);
+
+        if (foundUser == null) {
+            throw new NotFoundException("User with ID of " + id + " was not found!");
+        }
+
+        return foundUser;
+    }
+
+    //EXCEPTION HANDLERS
+
+    @ExceptionHandler
+    void handleUserNotFound(
+            NotFoundException exception,
+            HttpServletResponse response) throws IOException {
+
+        response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
     }
 }
