10e64d093ad41d77b50cf3215ddd891a642e9f3e
jmbothe <jeffrymbothe@gmail.com>
2018-03-25 16:05:38 -0400

    define ui routes

--- portal-ui/src/browse.js
+++ portal-ui/src/browse.js
@@ -25,3 +25,64 @@ module.exports = {media, regions};
 //     console.log(body)
 //   })
 // })
+
+// async componentDidMount() {
+//   const promises = [1,2,3,4,5,6].map(item => {
+//     return fetch(`http://api.thewalters.org/v1/collections/2/objects?page=${item}&pageSize=100&apikey=DUpHWeGMjW4pDlIs7DmTeHMj4Wt0LlqEd7BMJiXviUx3HsLDrZ7kZRQv86C0lF0y`)
+//       .then(response => {
+//         return response.json();
+//       })
+//   })
+  
+//   Promise.all(promises).then(bodies => {
+//     console.log(bodies)
+//     const objects = bodies.reduce((acc, body) => {
+//       acc = acc.concat(body.Items);
+//       return acc;
+//     }, []);
+
+//     console.log(objects.sort((a,b) => a.DateEndYear - b.DateEndYear))
+
+//     const creators = objects.reduce((acc, obj) => {
+//       if (!acc[obj.Creator]) {
+//         acc[obj.Creator] = 1;
+//       } else {
+//         acc[obj.Creator]++;
+//       }
+//       return acc
+//     }, {})
+//     console.log(creators)
+
+//     const classifications = objects.reduce((acc, obj) => {
+//       if (!acc[obj.Classification]) {
+//         acc[obj.Classification] = 1;
+//       } else {
+//         acc[obj.Classification]++;
+//       }
+//       return acc
+//     }, {})
+//     console.log(classifications)
+
+//     const periods = objects.reduce((acc, obj) => {
+//       if (!acc[obj.Period]) {
+//         acc[obj.Period] = 1;
+//       } else {
+//         acc[obj.Period]++;
+//       }
+//       return acc
+//     }, {})
+//     console.log(periods)
+
+//     const geographies = objects.reduce((acc, obj) => {
+//       obj.GeoIDs.split(', ').forEach(id => {
+//         if (!acc[id]) {
+//           acc[id] = 1;
+//         } else {
+//           acc[id]++;
+//         }
+//       })
+//       return acc
+//     }, {})
+//     console.log(geographies)
+//   })     
+// }
