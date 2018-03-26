ac5936b8ecdc5a6cb62b5b4aea88858cb659a1c6
jmbothe <jeffrymbothe@gmail.com>
2018-03-26 11:11:13 -0400

    add Search inputs

--- portal-ui/src/browse.js
+++ portal-ui/src/browse.js
@@ -17,7 +17,65 @@ const regions = {
   'North America & the Caribbean': ['america', 'dominican']
 };
 
-module.exports = {media, regions};
+const cultures = ['Ecuadorian','Valdivia','Ecuadorian (Valdivia style)','Native North American',
+  'Olmec','Tlatilco','Xochipala','Mexican','Mexican (Xochipala)',
+  'Mexican (Olmec style)','Chorrera','Sultepec','Chavín','Zapotec','Peruvian (Cupisnique)','Tembladera',
+  'Chupícuaro','Pre-Columbian','Colombian','Salinar','Maya','Jalisco','Mexican (Mezcala style)',
+  'Colombian (San Agustin)','Mexican (Chontal style)','Colima','Mexican (Colima-Nayarit)',
+  'Mexican (Jalisco style)','Nayarit','Vicús','Arenal','Zacatecas','Mexican (Colima style)',
+  'Mexican (Colima-Jalisco)','Nazca','Mexican (Olmec-early Teotihuacan style)','Colima (Colima-Michoacán)',
+  'Mezcala','Guangala','Moche','Costa Rican (Atlantic Watershed style)','Costa Rican (Atlantic Watershed)',
+  'Costa Rican','Costa Rican (Guanacaste-Nicoya style)','Mexican (Mezcala)',
+  'Guanacaste-Nicoya','Recuay','Costa Rican (Central Pacific area style)','Peruvian (Moche style)',
+  'Ecuadorian (Tumaco-La Tolita)','Mexican (Teotihuacan style)','Jama-Coaque','Teotihuacán',
+  'Mexican (Monte Albán style)','Veracruz','Huari','Mexican (Remojades or Nopiloa)',
+  'Panamanian (Joaquín Polychrome)','Calima','Michoacan','Peruvian (Tiwanaku-Moquegua)'
+  ,'Huastec','Quiché (Maya)',
+  'Mexican (Jaina)','Guatemalan (Mayan style)','Guatemalan (Maya style)',
+  'Remojadas','Mexican (Nopiloa)','Mexican (Veracruz or Maya)','Guatemalan','Panamanian (Macaracas)',
+  'Colombian (Calima style)','Tairona','Tolima','Lambayeque (Sicán)',
+  'Peruvian (Sicán (Lambayeque) style)','Carchí','Mexican (Mixteca-Puebla style)',
+  'Costa Rican','Costa Rican (Potosí Appliqué)','Quimbaya (Caldas complex)',
+  'Mixteca-Puebla','Peruvian (Sicán-Chimú style)','Chancay','Chimú','Peruvian (Chimú style)','Chimú',
+  'Nariño (Tuza)','Nariño','Panamanian (Coclé style)','Mixtec','Panamanian (Gran Chiquíri)',
+  'Taino','Central American','Diquís','Manteño','Tarascan',
+  'Colombian (Chimila)','Coclé','Chiriqui (Panamanian)','Aztec','Panamanian',
+  'Panamanian (Veraguas-Gran Chiriquí or Coclé)','Peruvian','Coclé',
+  'Panamanian (Gran Chiriquí)','Aztec (Mexica)','Mexican (Toltec or Aztec style)',
+  'Mexican (Aztec style)','Mexican (Zacatecas style)','Mexican (Maya-Toltec)',
+  'Maya','Inca','Peruvian (Inka style)','Panamanian (Veraguas style)',
+  'Sinú','Colombian (Zenú (Sinú)-Darién)','Colombian (Zenú (Sinú)-Darién style)',
+  'Colombian (Moskito)','Mexican (Kino Viejo-Seri)',
+  'Peruvian (Nazca style)'
+]
+
+const periods = ["Formative","late Archaic","Archaic-Early Formative","Early Formative","Early Pre-Classic",
+  "Early-Middle Formative","Middle Formative","Late Formative","Early Horizon-Early Intermediate",
+  "Initial Period-Early Horizon","Monte Albán I","Late Initial-Early Horizon","Late Preclassic",
+  "Late Formative-Early Classic","Horqueta","Archaic","Late Pre-Classic","Comala Phase","Protoclassic",
+  "Early Intermediate Phases II-III","Comala Phase","Late Preclassic-Early Classic","Early Classic",
+  "Terminal Formative-Early Classic","Early Intermediate","Period IV","late Period IV",
+  "Middle Classic","Early Intermediate Phases III-IV","Early Classic","Phases III-IV",
+  "Early Intermediate Phases IIII-IV","Early Intermediate (Phase 6)","Late Classic, Monte Albán IIIb",
+  "late Period IV-early Period V","Early Intermediate-Middle Horizon","Middle Horizon","Late Classic","Yotoco",
+  "Late Formative-Classic","Late Preclassic-Classic","Monte Albán IIIB-IV","Pre-Colombian",
+  "late Period V-early Period VI","Early Postclassic","Late Classic-Early Postclassic","Early Postclassic",
+  "Middle Horizon-Late Intermediate","Period VI","Period V–VI","late Period VI","Late Postclassic",
+  "Late Intermediate","Late Period","Late Post-Classic","Late Period V-Period VI",
+  "Late Intermediate-Late Horizon","Late Period IV–VI","Postclassic","Period V-VI",
+  "Pre-Early Conquest","Classic-Postclassic","Late Horizon","Late Period V–Period VI","Colonial"
+]
+
+const orderBy = {
+  Title: 'Title',
+  Culture: 'Creator',
+  'Date Range End': 'DateBeginYear',
+  'Date Range Begin': 'DateEndYear',
+  Media: 'Calssification',
+  Period: 'Period'
+}
+
+module.exports = {media, regions, cultures, periods, orderBy};
 
 // fetch(`http://api.thewalters.org/v1/objects?collectionId=2&creator=${regions['North America & the Caribbean'].join(',')}&pageSize=100&apikey=DUpHWeGMjW4pDlIs7DmTeHMj4Wt0LlqEd7BMJiXviUx3HsLDrZ7kZRQv86C0lF0y`)
 // .then(response => {
--- portal-ui/src/components/home/Search.js
+++ portal-ui/src/components/home/Search.js
@@ -1,11 +1,82 @@
 import React, { Component } from 'react';
 
+import {cultures, periods, regions, media, orderBy} from '../../browse'
+
 class Search extends Component {
-  state = {}
+  state = {
+    search: {
+
+    }
+  }
   render() { 
+    const orderByOptions = Object.entries(orderBy).map(item => {
+      return <option value={item[1]}>{item[0]}</option>
+    });
+
+    const cultureOptions = cultures.map(item => {
+      return <option value={item}>{item}</option>
+    });
+
+    const periodOptions = periods.map(item => {
+      return <option value={item}>{item}</option>
+    });
+
+    const mediaOptions = media.map(item => {
+      return <option value={item}>{item}</option>
+    });
+
     return (
       <section className="search-container">
-        <h3>Search</h3>
+        <header>
+          <h3>Search</h3>
+        </header>
+        <section>
+          <form className="search-form">
+            <div className="form-row">
+              <label htmlFor="Title">Title</label>
+              <input
+                type="text" id="Title" name="Title"
+                onChange={this.onChange}
+                />
+            </div>
+            <div className="form-row">
+              <label htmlFor="DateBeginYear">Date Range</label>
+              <input
+                type="number" id="DateBeginYear" name="DateBeginYear"
+                onChange={this.onChange}
+              />
+              <input
+                type="number" id="DateEndYear" name="DateEndYear"
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="Creator">Culture</label>
+              <select id="Creator" name="Creator">
+                {cultureOptions}
+              </select>
+            </div>
+            <div className="form-row">
+              <label htmlFor="Period">Period</label>
+              <select id="Period" name="Period">
+                {periodOptions}
+              </select>
+            </div>
+            <div className="form-row">
+            <label htmlFor="Classification">Media</label>
+              <select id="Classification" name="Classification">
+                {mediaOptions}
+              </select>
+            </div>
+            <div className="form-row">
+            <label htmlFor="OrderBy">Order By</label>
+              <select id="OrderBy" name="OrderBy">
+                {orderByOptions}
+              </select>
+            </div>
+          </form>
+        </section>
+        
       </section>
     )
   }
