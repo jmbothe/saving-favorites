710a8bf4410a9361ec66ce21c0f218d46dc84132
jmbothe <jeffrymbothe@gmail.com>
2018-03-27 17:49:42 -0400

    bugfix null favorites in Detail component

--- portal-ui/src/components/detail/Detail.js
+++ portal-ui/src/components/detail/Detail.js
@@ -18,7 +18,7 @@ class Detail extends Component {
     if (this.state.redirect) return <Redirect to={`/${this.state.redirect}`}/>;
 
     const fav =
-      this.props.currentUser
+      this.props.currentUser.favorites
       ? this.props.currentUser.favorites.find(item => item.itemId == this.props.detail.ObjectID)
       : null;
 
